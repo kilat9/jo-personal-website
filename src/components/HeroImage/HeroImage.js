30648554c40432f75e600403118987662a248e99
Johari Idzman <johariidzman@gmail.com>
2023-01-09 13:55:08 +0800

    changed image filepath

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 var index;
 const HeroImage = () => {
-  const images = ["./jo-personal-website/images/Jo2.png", "./jo-personal-website/images/Jo.png"];
+  const images = ["./images/Jo2.png", "./images/Jo.png"];
   const [image, setImage] = useState(images[0]);
   // var timer
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Flashguard - Melhack 2021",
     description:
       "FlashGuard is a Chrome extension that would tell if a Netflix show was safe to watch for people with photosensitive epilepsy. I worked on the companion website that would display the list of Unsafe shows using the Does the dog die API.",
-    image: "./jo-personal-website/images/flashguard.png",
+    image: "./images/flashguard.png",
     tags: ["Chrome-Extension", "React", "Node"],
     source: "https://github.com/Diskordlit/FlashGuard",
     visit: "https://devpost.com/software/flashguard",
@@ -13,7 +13,7 @@ export const projects = [
     title: "Machine Spirit - Discord Bot",
     description:
       "Machine Spirit is a Discord bot developed in Python. The bot can play pre-selected songs and has an 8-ball module that answers questions.",
-    image: "./jo-personal-website/images/MachineSpirit.png",
+    image: "./images/MachineSpirit.png",
     tags: ["Python", "Discord.py"],
     source: "https://github.com/kilat9/machine_spirit-Discord_Bot",
     visit: "https://github.com/kilat9/machine_spirit-Discord_Bot",
@@ -23,7 +23,7 @@ export const projects = [
     title: "Libsys : Library Management System",
     description:
       "This project is a library book management developed for the SDP module. I worked on the registration and account management feature using HTML and PHP.",
-    image: "./jo-personal-website/images/Libsys.png",
+    image: "./images/Libsys.png",
     tags: ["HTML", "CSS", "JavaScript"],
     source: "https://github.com/Diskordlit/libsys",
     visit: "https://github.com/Diskordlit/libsys",
@@ -33,7 +33,7 @@ export const projects = [
     title: "TixEdu - Online Learning Website",
     description:
       "This website is an online course education platform developed for the Web Development module. I worked on the back-end to allow users manage their accounts with CRUD functions implemented.",
-    image: "./jo-personal-website/images/TixEdu.png",
+    image: "./images/TixEdu.png",
     tags: ["HTML", "CSS", "JavaScript"],
     source: "https://github.com/kilat9/TixEdu-Website",
     visit: "https://github.com/kilat9/TixEdu-Website",
@@ -43,7 +43,7 @@ export const projects = [
     title: "VILB Library System (Java)",
     description:
       "This Library Management System was developed for the Java Programming module. It allows the user (Librarian) to register new users, add new books and track if a customer borrows and renew books.",
-    image: "./jo-personal-website/images/VILB-Java.png",
+    image: "./images/VILB-Java.png",
     tags: ["Java", "Netbeans Apache IDE"],
     source: "https://github.com/kilat9/LibraryManagementSystem",
     visit: "https://github.com/kilat9/LibraryManagementSystem",
@@ -53,7 +53,7 @@ export const projects = [
     title: "Cafeteria Management System",
     description:
       "This Cafeteria Management System was developed for the Visual Basic Programming module. Users were able to make reservations, take orders and view receipt history.",
-    image: "./jo-personal-website/images/vbn.png",
+    image: "./images/vbn.png",
     tags: ["Visual Basic", "VB.NET", "Visual Studio"],
     source: "https://github.com/kilat9/CafeteriaSystem-VisualBasic",
     visit: "https://github.com/kilat9/CafeteriaSystem-VisualBasic",
@@ -77,84 +77,84 @@ export const StackList = [
     title: "React.js",
     description:
       "React is a popular JS Framework for developing single-page web applications. I was first exposed to it during my internship and has now become my default stack for front-end web development.",
-    image: "./jo-personal-website/images/StackImage/React.png",
+    image: "./images/StackImage/React.png",
     id: "0",
   },
   {
     title: "Node.js",
     description:
       "Node.js allows Javascript code to be executed on the server-side. It is used as the base of various projects such as Flashguard and this portfolio website. ",
-    image: "./jo-personal-website/images/StackImage/Node.png",
+    image: "./images/StackImage/Node.png",
     id: "1",
   },
   {
     title: "HTML",
     description:
       "HTML is used to define the content of a webpage. Knowlwdge of HTML is an important part of JSX that incorporates HTML with Javascript.",
-    image: "./jo-personal-website/images/StackImage/HTML.png",
+    image: "./images/StackImage/HTML.png",
     id: "2",
   },
   {
     title: "CSS",
     description:
       "CSS is used for styling webpages and important for developing clean and responsive webpages.",
-    image: "./jo-personal-website/images/StackImage/CSS.png",
+    image: "./images/StackImage/CSS.png",
     id: "3",
   },
   {
     title: "Javascript",
     description:
       "Javascript is the backbone for web-development. It has become my main programming language.",
-    image: "./jo-personal-website/images/StackImage/JS.png",
+    image: "./images/StackImage/JS.png",
     id: "4",
   },
   {
     title: "PHP",
     description:
       "PHP is a server-side coding language. I have worked with PHP to send and retrieve data from a myPHPadmin database in my assignment projects.",
-    image: "./jo-personal-website/images/StackImage/PHP.png",
+    image: "./images/StackImage/PHP.png",
     id: "5",
   },
   {
     title: "Visual Basic (VB.NET)",
     description:
       "Visual Basic is an object-oriented language developed by Microsoft. I used Visual Basic with the .NET framework to build a Cafeteria management system using the Visual Studio GUI builder.",
-    image: "./jo-personal-website/images/StackImage/VS.png",
+    image: "./images/StackImage/VS.png",
     id: "6",
   },
   {
     title: "Python",
     description:
       "Python is a popular high level programming language because of its simple syntax. I have developed a Discord bot using Discord.py and currently developing a game using Pigame.",
-    image: "./jo-personal-website/images/StackImage/Python.png",
+    image: "./images/StackImage/Python.png",
     id: "7",
   },
   {
     title: "Java",
     description:
       "Java was the first programming language that I learnt in Highschool. I learned key OOP concepts such as inheritance and polymorphism in Java.",
-    image: "./jo-personal-website/images/StackImage/Java.png",
+    image: "./images/StackImage/Java.png",
     id: "8",
   },
   {
     title: "Netbeans",
     description:
       "Netbeans is a Java IDE. I used Netbeans with the GUI builder for the VILB Library Management System.",
-    image: "./jo-personal-website/images/StackImage/Netbeans.png",
+    image: "./images/StackImage/Netbeans.png",
     id: "9",
   },
   {
     title: "Git",
     description:
       "Git is a usefull Version Control System for managing your codes. I first learnt git during my internship and have applied the knowlwdge by maintaining various personal projects in Github repositories.",
-    image: "./jo-personal-website/images/StackImage/Git.png",
+    image: "./images/StackImage/Git.png",
     id: "10",
   },
   {
     title: "Raspberry Pi",
     description:
       "Raspberry Pi is a mini computer that has taught me the basics of Linux command line is currently used as a Web-server.",
-    image: "./jo-personal-website/images/StackImage/Raspberry.png",
+    image: "./images/StackImage/Raspberry.png",
     id: "11",
   },
 ];
@@ -163,21 +163,21 @@ export const accomplishments = [
   {
     title: "2nd Place For Melhack 2021",
     organization: "Melbourne University",
-    image: "./jo-personal-website/images/flashguard.png",
+    image: "./images/flashguard.png",
   },
   {
     title: "National Champion CISCO Packet Tracer Competition 2021",
     organization: "CISCO",
-    image: "./jo-personal-website/images/CISCO.png",
+    image: "./images/CISCO.png",
   },
   {
     title: "Head of Youth Development",
     organization: "APU Student Activities and Representative Council (SARC)",
-    image: "./jo-personal-website/images/SARC.png",
+    image: "./images/SARC.png",
   },
   {
     title: "School Prefect",
     organization: "Victoria Institution Prefects' Board of 2018",
-    image: "./jo-personal-website/images/VIPB.png",
+    image: "./images/VIPB.png",
   },
 ];
